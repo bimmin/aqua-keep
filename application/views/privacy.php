7429b0e87f0bf4c809562c70ae3c0a94c0986fe4
David's Codes <david.ethier@gmail.com>
2014-10-14 17:18:19 -0700

    Update privacy.php
    Added code to make footer login link properly work.

--- application/views/privacy.php
+++ application/views/privacy.php
@@ -8,6 +8,34 @@
     <link href="../../assets/css/foundation.css" rel="stylesheet" />
     <link rel="stylesheet" href="../../assets/css/mystyle.css" />
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $("#login").on("submit", function(){
+            var form = $(this);
+            $.post(form.attr("action"), form.serialize(), function(data){
+      
+              if (data['errors'] == true){
+                      $("#errors").html(data['messages']);
+                    }
+                    else{
+                       location.href="../user/profile";
+                    }
+            }, "json");
+        return false;
+        });
+      
+        $("#register").on("submit", function(){
+            var form = $(this);
+            $.post(form.attr("action"), form.serialize(), function(html){
+      
+              // console.log(html);
+              $("#errors").html(html);
+            }, "json");
+        return false;
+        });
+      
+      });
+    </script>
   </head>
   <body>
     <div class="wrapper">
@@ -27,7 +55,7 @@
       <div class="row">
         <div class="large-12 columns">
           <h3>Privacy is my policy!</h3>
-          <p>When you register for an account on aqua-keep.com we ask only for an email address, password and display name. Your email address will be used to login to the website but is not used for any other purposes. Your email address will not dispaly anywhere on this website nore will we ever share or sell your email address. We do not ask for your name or any other personal informaion. You may use your real name on aqua-keep.com but will never require you to. Any info, including photos and comments made on an aquarium page are availble for public viewing.</p>
+          <p>When you register for an account on aqua-keep.com we ask only for an email address, password and display name. Your email address will be used to login to the website but is not used for any other purposes. Your email address will not dispaly anywhere on this website and we will not share or sell your email address. We do not ask for your name or any other personal informaion. You may use your real name on aqua-keep.com but will never require you to. Any info, including photos and comments made on an aquarium page are availble for public viewing. Enjoy the site!</p>
           <p class="text-center"><img src="<?php echo base_url("/assets/img/clownfish.jpg");?>"></p>
         </div>
       </div>
@@ -50,8 +78,29 @@
         </div>
       </footer>
     </div>
+    <div data-reveal-ajax="true" id="myModal" class="reveal-modal small" data-reveal>
+      <div id="errors"></div>
+      <h3>Already have an account? Login here:</h3>
+      <form id="login" action="/user/process_login" method="post">
+        <!--  <input type="hidden" name="action" value="login" /> -->
+        <input type="text" name="email" placeholder="Email address" />
+        <input type="password" name="password" placeholder="Password" />
+        <input type="submit" value="Login" />
+      </form>
+      <h3>Create a new account</h3>
+      <form id="register" action="/user/process_registration" method="post">
+        <input type="hidden" name="action" value="register" />
+        <input type="text" name="display_name" placeholder="Display Name" /><br />
+        <input type="text" name="email" placeholder="Email address" /><br />
+        <input type="password" name="password" placeholder="Password" /><br />
+        <input type="password" name="confirm_password" placeholder="Confirm Password" /><br />
+        <input type="submit" value="Register" />
+      </form>
+      <a class="close-reveal-modal">×</a>
+    </div>
     <script src="/assets/js/foundation/foundation.js"></script>
     <script src="/assets/js/foundation/foundation.topbar.js"></script>
+    <script src="/assets/js/foundation/foundation.reveal.js"></script>
     <script>
       $(document).foundation();
     </script>
